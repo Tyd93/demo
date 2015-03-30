17a008040da341fa2e105a0213c7bd1a0edf7335
tyd93 <sebak93@gmail.com>
2015-03-30 12:53:06 -0300

    Fix #1

--- lista.h
+++ lista.h
@@ -105,7 +105,7 @@ Elemento *suprimeElementoLista(Lista *unaLista, unsigned int posicion){
 int muestraLista(Lista *unaLista){
 	//Si no hay elementos retornar error (0)
 	if (estaVaciaLista(unaLista)) {
-		printf("No hay palabras para mostrar (cola vacía).\n");
+		printf("No hay elementos en la lista.\n");
 		return(0);
 	}
 	printf("Lista:\n");
